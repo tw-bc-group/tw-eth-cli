fdae359641fc6567e38c0f9c890fbdfb8b82dcd8
yin <yin.shen@thoughtworks.com>
2020-09-03 18:19:32 +0800

    feat: gen compressed public key

--- src/inspects.js
+++ src/inspects.js
@@ -1,17 +1,21 @@
-exports.privateKeyToPublicKey = function(privateKey) {
-  var elliptic = require("elliptic"); 
-  var secp256k1 = new elliptic.ec("secp256k1");
-  var web3 = require("web3");
+exports.privateKeyToPublicKey = function (privateKey) {
+    var elliptic = require("elliptic");
+    var secp256k1 = new elliptic.ec("secp256k1");
+    var web3 = require("web3");
 
-  let buffer = Buffer.from(privateKey.slice(2), "hex");
+    let buffer = Buffer.from(privateKey.slice(2), "hex");
 
-  let ecKey = secp256k1.keyFromPrivate(buffer);
-  let publicKey = "0x" + ecKey.getPublic(false, 'hex').slice(2);
-  let address = "0x" + web3.utils.keccak256(publicKey).slice(-40);
+    let ecKey = secp256k1.keyFromPrivate(buffer);
+    let publicKey = "0x" + ecKey.getPublic(false, 'hex').slice(2);
+    let address = "0x" + web3.utils.keccak256(publicKey).slice(-40);
 
-  return {
-    publicKey: publicKey,
-    address: address
-  };
+    const ethers = require('ethers');
+    const wallet = new ethers.Wallet(privateKey);
+
+    return {
+        publicKey: wallet.signingKey.keyPair.publicKey || publicKey,
+        compressedPublicKey: wallet.signingKey.keyPair.compressedPublicKey,
+        address: wallet.signingKey.address || address
+    };
 }
 
